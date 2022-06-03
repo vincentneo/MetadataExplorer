7160bb828dd1a7c70b4c93d8c54c8d3bb306849a
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2022-06-04 00:12:46 +0800

    Initial Commit

---
+++ MetadataExplorer/MetadataExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetadataExplorerApp.swift
+//  MetadataExplorer
+//
+//  Created by Vincent Neo on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MetadataExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
